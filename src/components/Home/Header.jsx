7db097ff3b12338938d18237d5a2071aee0ee0d5
STharshan <sivatharshan81@gmail.com>
2025-10-22 17:04:22 +0530

    a

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -16,6 +16,7 @@ export default function HeroSection() {
         src="/video.mp4"
         autoPlay
         loop
+         playsInline
         muted
       ></video>
 
@@ -26,7 +27,7 @@ export default function HeroSection() {
       <div className="relative z-10 flex flex-col items-center justify-center text-center h-full px-4">
         {/* Title */}
         <h1
-          className="text-[85px] font-bold tracking-widest mb-2 text-primary dark:text-primary"
+          className="text-5xl font-bold tracking-widest mb-2 text-primary dark:text-primary"
           data-aos="fade-up"
         >
           SterlingSpace
